17c075e38a5c1ff050d0fdc18d99a08b5844b5fb
Eduardo Rosendo <eduardojra96@gmail.com>
2022-11-21 17:01:31 -0400

    feat(song): Add funtion view to render audio features

--- sa/songs/views.py
+++ sa/songs/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
-from .utils import search_song_by_name
+from .utils import search_song_by_name, search_audio_features
 
 
 def index(request):
     search_tex = request.GET.get('search')
-    print(search_tex)
     songs = search_song_by_name(search_tex)
     template = 'index.html'
     if request.htmx:
         template = 'search/partials/results.html'
     return render(request, template, context={ 'songs':songs})
+
+def details(request, song_uri):
+    template = 'details.html'
+    song = search_audio_features(song_uri)
+    return render(request, template, context={ 'song':song})
