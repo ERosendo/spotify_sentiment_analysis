c8ac3cd9a0fd4a3b7aae02cc018f5fb73408bf5e
Eduardo Rosendo <eduardojra96@gmail.com>
2022-11-25 23:44:29 -0400

    feat(moods): Add separate endpoint to compute the mood

--- sa/songs/urls.py
+++ sa/songs/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<slug:song_uri>', views.details, name='detail'),
+    path('<slug:song_uri>/mood/', views.moods, name='mood_for_song')
 ]
