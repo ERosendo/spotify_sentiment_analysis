4cb79111cf0ddb1b420b772b7e655ceb919e316e
Eduardo Rosendo <eduardojra96@gmail.com>
2022-11-25 23:45:54 -0400

    feat(mood): Add function to handle mood endpoint

--- sa/songs/views.py
+++ sa/songs/views.py
@@ -1,5 +1,6 @@
 from utils import spotiscience
 from django.shortcuts import render
+from django.http import HttpResponse
 from utils.spotiscience import CREDENTIALS
 
 from .utils import search_audio_features, search_song_by_name
@@ -13,10 +14,14 @@ def index(request):
     return render(request, template, context={ 'songs':songs})
 
 def details(request, song_uri):
-    sp = spotiscience.SpotiSciencePredicter()
-    sd = spotiscience.SpotiScienceDownloader(credentials=CREDENTIALS)
     template = 'details.html'
     song = search_audio_features(song_uri)
+    return render(request, template, context={ 'song':song })
+
+
+def moods(request, song_uri):
+    sp = spotiscience.SpotiSciencePredicter()
+    sd = spotiscience.SpotiScienceDownloader(credentials=CREDENTIALS)
     song_to_analyze = sd.get_song_features(song_id=song_uri)
     mood = sp.predict_song_mood(song=song_to_analyze)
-    return render(request, template, context={ 'song':song, 'mood': mood})
+    return HttpResponse(f'{mood}')
