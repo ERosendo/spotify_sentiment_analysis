2cb954d9ab4ce438b6de3c52849696a9878aa3a0
Eduardo Rosendo <eduardojra96@gmail.com>
2022-12-07 18:31:14 -0400

    feat(pdf): Register view to create a pdf report

--- sa/songs/urls.py
+++ sa/songs/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<slug:song_uri>', views.details, name='detail'),
-    path('<slug:song_uri>/mood/', views.moods, name='mood_for_song')
+    path('<slug:song_uri>/mood/', views.moods, name='mood_for_song'),
+    path('<slug:song_uri>/pdf/', views.some_view, name='pdf_report')
 ]
