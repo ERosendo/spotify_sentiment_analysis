9c4170169b384bf8441bfbb773c889d0601f64f7
Eduardo Rosendo <eduardojra96@gmail.com>
2022-12-07 18:29:40 -0400

    feat(utils): Add method to get song data from db

--- sa/songs/utils.py
+++ sa/songs/utils.py
@@ -34,7 +34,7 @@ AUDIO_FEATURES_FOR_POLAR_GRAPH = ['acousticness',
                                 'valence']
 
 def extract_song_data(track) -> Song:
-    artist = [Artist(track['uri'], x['name']) for x in track['artists']]
+    artist = [Artist(x['uri'], x['name']) for x in track['artists']]
     album = Album(track['album']['uri'], track['album']
                   ['name'], track['album']['images'][0]['url'])
     uri = track['uri'].split(':')[-1]
@@ -43,7 +43,11 @@ def extract_song_data(track) -> Song:
 def extract_audio_features(raw_features):
     labels = []
     data = []
-    for k, v in raw_features.items():
+    if isinstance(raw_features, dict):
+        feature = raw_features
+    else:
+        feature = raw_features.__dict__
+    for k, v in feature.items():
         if k in AUDIO_FEATURES_FOR_POLAR_GRAPH:
             labels.append(k)
             data.append(v)
@@ -70,4 +74,11 @@ def search_audio_features(uri: str):
     raw_song_data = sp_client.get_song_data(f'spotify:track:{uri}')
     song_data = extract_song_data(raw_song_data)
     label , values = extract_audio_features(raw_audio_features)
+
     return {'data': song_data, 'audio_features': raw_audio_features, 'labels': label, 'feature_value': values}
+
+
+def get_song_from_db(db_song):
+    label, values = extract_audio_features(db_song.audio_features)
+    song_data = { 'name': db_song.name, 'uri': db_song.uri, 'album': db_song.album , 'artist':db_song.album.artist }
+    return {'data': song_data, 'audio_features': db_song.audio_features, 'labels': label, 'feature_value': values}
