c4ba52edb3200ede323245a5fb27a74e616416c1
Eduardo Rosendo <eduardojra96@gmail.com>
2022-12-07 18:29:08 -0400

    feat(songs): Add models to song app

--- sa/songs/models.py
+++ sa/songs/models.py
@@ -1,3 +1,36 @@
+from core.models import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class Artist(BaseModel):
+    uri = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+
+
+class Album(BaseModel):
+    artist = models.ManyToManyField(Artist)
+    uri = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+    cover = models.CharField(max_length=256)
+
+
+class Song(BaseModel):
+    album = models.ForeignKey(
+        Album,
+        related_name='songs',
+        on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    uri = models.CharField(max_length=256)
+    mood = models.CharField(max_length=256, null=True)
+    
+class AudioFeature(BaseModel):
+    song = models.OneToOneField(Song, related_name='audio_features', on_delete=models.CASCADE)
+    acousticness = models.FloatField(null=True, blank=True, default=None)
+    danceability = models.FloatField(null=True, blank=True, default=None)
+    energy = models.FloatField(null=True, blank=True, default=None)
+    instrumentalness = models.FloatField(null=True, blank=True, default=None)
+    liveness = models.FloatField(null=True, blank=True, default=None)
+    speechiness = models.FloatField(null=True, blank=True, default=None)
+    loudness = models.FloatField(null=True, blank=True, default=None)
+    valence = models.FloatField(null=True, blank=True, default=None)
+    tempo = models.FloatField(null=True, blank=True, default=None)
